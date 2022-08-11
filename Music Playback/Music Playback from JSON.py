fd89942f00dcaaf5d2996ea871b9ca4d28851abd
Chris Norcross <norcross@dbt-ethz.ch>
2022-08-11 17:12:55 +0200

    0.3.0
    + checks to prevent writing over files
    + input prompts for playback file
    + proper while looping function for Measuring file
    + added failsafe and redundant failsafe to prevent infinite while loop
    + reassigned tolerable_range variable to tolerable_dist
    + reassigned tol_dist variable to tol_range

--- Music Playback/Music Playback from JSON.py
+++ Music Playback/Music Playback from JSON.py
@@ -4,9 +4,9 @@ import os
 import json
 
 data_Path = os.path.dirname(__file__)
-
-JSON_Name = 'test_8-9-2022.json' # JSON file from 'Musically Responsive Measuring.py'
-WAV_Name = '8-9.wav' # file name for .wav audio file
+JSON_Name = input("\nPlease type name of JSON file, don't include '.json'.\nPress Enter to continue\n") # JSON file from 'Musically Responsive Measuring.py'
+JSON_Name += '.json'
+save_wav = int(input("\nType 1 to save a .wav file of the fabrication audio, otherwise type 0.\nPress Enter to continue\n")) # toggle to save playback as .wav file or not
 
 player = Player()
 player.open_stream()
@@ -30,8 +30,17 @@ for freq in JSON_dict['fab_tones']:
         player.play_wave(synthesizer.generate_constant_wave(freq, JSON_dict['audio_time']))
         wave.append(synthesizer.generate_constant_wave(freq, JSON_dict['audio_time']))
 
+def file_check(exp_path): # check for existing file with the same name
+    if os.path.exists(exp_path) == True: # if same named file exists, adds "- Copy" to the end
+        ext_len = 4 # this value is the length of the file extension (ex: '.json' = 5, '.wav' = 4)
+        exp_path = exp_path[0:-ext_len] + " - Copy" + exp_path[-ext_len:]
+        exp_path_new = file_check(exp_path)
+        return(exp_path_new)
+    elif os.path.exists(exp_path) == False:
+        return(exp_path)
 
 # export wav file
-export_Path = data_Path + '/' + WAV_Name
-
-writer.write_waves(export_Path, *wave)
+if save_wav == 1:
+    WAV_Name = input("\nPlease type name of wav file, don't include '.wav'.\nPress Enter to continue\n") # file name for .wav audio file
+    export_Path = file_check(data_Path + '/' + WAV_Name + '.wav')
+    writer.write_waves(export_Path, *wave)
--- Musical Measuring/Musically Responsive Measuring.py
+++ Musical Measuring/Musically Responsive Measuring.py
@@ -9,7 +9,7 @@ import time
 
 DATA_PATH = os.path.dirname(__file__)
 GH_JSON = 'JSON_from_GH.json' # name of JSON file from Grasshopper
-JSON_name = 'test_8-9-2022.json' # name to save JSON file as
+JSON_name = 'test_8-11-2022.json' # name to save JSON file as
 
 player = Player()
 player.open_stream()
@@ -20,7 +20,7 @@ fab_dists = [] # empty list to append measured distances to
 fab_tones = [] # empty list to append measurement tones to
 tolerance_counter = [] # empty list for counting number of consecutive times a measured distance is in a range
 
-audio_range = [200, 700] # comfortable range for audio scale in herz
+audio_range = [100, 600] # comfortable range for audio scale in herz
 audio_time = 0.1 # how long each audio tone is played for, also determines how often a measurement is taken
 
 # all distance units are in meters
@@ -31,16 +31,16 @@ with open(filename, 'r') as f:
     GH_JSON_dict = json.load(f) # open and load JSON file from Grasshopper
 
 target_dist = float(GH_JSON_dict['target_dist']) # the target distance
-tolerable_range = float(GH_JSON_dict['tolerable_range']) # value +/- the target_dist for an acceptable tolerance range
+tolerable_dist = float(GH_JSON_dict['tolerable_dist']) # value +/- the target_dist for an acceptable tolerance range
 toleration_times = int(GH_JSON_dict['toleration_times']) # number of times the distance measurement needs to be within the tolerance range
-tol_dist = [(target_dist - tolerable_range), (target_dist + tolerable_range)]
+tol_range = [(target_dist - tolerable_dist), (target_dist + tolerable_dist)]
 
 def findEvo():
     # Find Live Ports, return port name if found, NULL if not
     print('Scanning all live ports on this PC')
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
-        # print p # This causes each port's information to be printed out.
+        # print(p) # This causes each port's information to be printed out.
         if "5740" in p[2]:
             print('Evo found on port ' + p[0])
             return p[0]
@@ -94,69 +94,87 @@ def get_evo_range(evo_serial):
 def remap(old_val, old_min, old_max, new_min, new_max):
     return ((new_max - new_min) * (old_val - old_min) / (old_max - old_min) + new_min)
 
-if __name__ == "__main__":
-
+def measureLoop(): # connects to evo sensor, then processes measurements until tolerance_counter == toleration_times
+    # connect to evo sensor
     print('Starting Evo data streaming')
-    # Get the port the evo has been connected to
+    # get the port the evo has been connected to
     port = findEvo()
-
     if port == 'NULL':
         print("Sorry couldn't find the Evo. Exiting.")
         sys.exit()
     else:
         evo = openEvo(port)
+    
+    tolerance_counter = []
+    while len(tolerance_counter) < toleration_times: # measure and play audio loop
+        if len(tolerance_counter) > toleration_times: # extra failsafe clause just to make me feel safe
+            evo.close()
+            break
+        
+        evo.flushInput()
+        evo.flushOutput() # clears the serial buffer for low latency measuring
+        dist = get_evo_range(evo) # gets measurement
+        
+        if type(dist) is not str: # test for errors (measurement outside sensor limits or unable to measure)
+            if dist > tol_range[0] and dist < tol_range[1]: # test if 'dist' is in 'tol_range'
+                # if True, play beeping sound and lengthen 'tolerance_counter' list
+                player.play_wave(synthesizer2.generate_constant_wave(700, audio_time / 2))
+                player.play_wave(synthesizer2.generate_constant_wave(100, audio_time / 2))
+                fab_tones.append(700)
+                fab_tones.append(100)
+                fab_dists.append(dist)
+                tolerance_counter.append(1)
+                print(tolerance_counter)
+            
+            else:
+                # if 'dist' is outside tolerance range, then reset 'tolerance_counter' to an empty list
+                # generate and play tone corresponding to proximity of 'dist' to 'tol_range'
+                # a 'dist' close to 'tol_range' will have a high pitch tone, a 'dist' far from 'tol_range' will have a low pitch tone
+                fab_dists.append(dist)
+                tolerance_counter = []
+                if dist < tol_range[0]: # True if 'dist' is less than low end of 'tol_range'
+                    tone = remap(dist, measure_range[0], tol_range[0], audio_range[0], audio_range[1])
+                    player.play_wave(synthesizer.generate_constant_wave(tone, audio_time))
+                    fab_tones.append(tone)
+                if dist > tol_range[1]: # True if 'dist' is more than high end of 'tol_range'
+                    tone = remap(dist, tol_range[1], measure_range[1], audio_range[1], audio_range[0])
+                    player.play_wave(synthesizer.generate_constant_wave(tone, audio_time))
+                    fab_tones.append(tone)
+        print(dist)
+    evo.close()
+
+def file_check(exp_path): # check for existing file with the same name
+    if os.path.exists(exp_path) == True: # if same named file exists, adds "- Copy" to the end
+        ext_len = 5 # this value is the length of the file extension (ex: '.json' = 5, '.wav' = 4)
+        exp_path = exp_path[0:-ext_len] + " - Copy" + exp_path[-ext_len:]
+        exp_path_new = file_check(exp_path)
+        return(exp_path_new)
+    elif os.path.exists(exp_path) == False:
+        return(exp_path)
+
+if target_dist < measure_range[0] or target_dist > measure_range[1]: # measurable distance failsafe
+    print("ERROR!!!\nERROR!!!\nERROR!!!\ntarget_dist is outside measure_range")
+    sys.exit()
 
 start_time = time.monotonic()
-
-for i in range(250):
-    pass
-    evo.flushInput()
-    evo.flushOutput() # clears the serial buffer for low latency measuring
-    dist = get_evo_range(evo) # gets measurement
-    
-    if type(dist) is not str: # test for errors (measurement outside sensor limits or unable to measure)
-        if dist > tol_dist[0] and dist < tol_dist[1]: # test if 'dist' is in tolerance range
-            # if True, play beeping sound and lengthen 'tolerance_counter' list
-            player.play_wave(synthesizer2.generate_constant_wave(700, audio_time / 2))
-            player.play_wave(synthesizer2.generate_constant_wave(100, audio_time / 2))
-            fab_tones.append(700)
-            fab_tones.append(100)
-            fab_dists.append(dist)
-            tolerance_counter.append(1)
-            print(tolerance_counter)
-            
-            if len(tolerance_counter) >= toleration_times: # check number of consecutive measurements in 'tolerance_counter'
-                # if length of 'tolerance_counter' == 'toleration_times', then break the loop
-                evo.close()
-                break
-        
-        else:
-            # if 'dist' is outside tolerance range, then reset 'tolerance_counter' to an empty list and play tone corresponding to 'dist'
-            tone = remap(dist, measure_range[0], measure_range[1], audio_range[0], audio_range[1])
-            player.play_wave(synthesizer.generate_constant_wave(tone, audio_time))
-            tolerance_counter = []
-            fab_tones.append(tone)
-            fab_dists.append(dist)
-    print(dist)
-
+measureLoop()
 end_time = time.monotonic()
 
 # compile fabrication related data as dictionary and save as a JSON file
 if __name__ == "__main__":
     JSON_dict = {}
     JSON_dict["target_dist"] = target_dist
-    JSON_dict["tolerable_range"] = tolerable_range
+    JSON_dict["fab_time"] = end_time - start_time
+    JSON_dict["tolerable_dist"] = tolerable_dist
     JSON_dict["toleration_times"] = toleration_times
     JSON_dict["measure_range"] = measure_range
     JSON_dict["audio_range"] = audio_range
     JSON_dict["audio_time"] = audio_time
-    JSON_dict["fab_time"] = end_time - start_time
     JSON_dict["fab_dists"] = fab_dists
     JSON_dict["fab_tones"] = fab_tones
 
-filename = os.path.join(DATA_PATH, JSON_name)
+filename = file_check(os.path.join(DATA_PATH, JSON_name))
 with open(filename, 'w') as f:
     f.write(json.dumps(JSON_dict, sort_keys=False)) # save data as JSON file
 
 print("Fabrication completed in " + str("%.3f" % (end_time - start_time)) + " seconds.")
-evo.close()
