30ae4c74a0d4f2fb5fd654f305378476e7c544a4
norcrossDBT <91247883+norcrossDBT@users.noreply.github.com>
2022-08-28 15:48:22 +0200

    0.3.2
    Small improvements and bug fixes

--- code/Music Playback from JSON.py
+++ code/Music Playback from JSON.py
@@ -3,7 +3,6 @@ import sys
 import os
 import json
 
-data_Path = os.path.dirname(__file__)
 JSON_Name = input("\nPlease type name of JSON file, don't include '.json'.\nPress Enter to continue\n") # JSON file from 'Musically Responsive Measuring.py'
 JSON_Name += '.json'
 save_wav = int(input("\nType 1 to save a .wav file of the fabrication audio, otherwise type 0.\nPress Enter to continue\n")) # toggle to save playback as .wav file or not
@@ -14,21 +13,25 @@ synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2
 synthesizer2 = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=True, osc2_waveform=Waveform.triangle)
 writer = Writer()
 
-filename = os.path.join(data_Path, JSON_Name)
+data_Path = os.path.dirname(__file__)
+read_data_Path = data_Path  + "\data\JSON_files"
+filename = os.path.join(read_data_Path, JSON_Name)
 
 with open(filename, 'r') as f:
     JSON_dict = json.load(f) # open and load JSON file
 
 wave = [] # empty list to append waves to
 
+note_time = JSON_dict['fab_time'] / len(JSON_dict['fab_dists'])
+
 # playback tones heard during fabrication
 for freq in JSON_dict['fab_tones']:
     if freq == 700 or 100:
-        player.play_wave(synthesizer2.generate_constant_wave(freq, JSON_dict['audio_time'] / 2))
-        wave.append(synthesizer2.generate_constant_wave(freq, JSON_dict['audio_time'] / 2))
+        player.play_wave(synthesizer2.generate_constant_wave(freq, note_time))
+        wave.append(synthesizer2.generate_constant_wave(freq, note_time))
     else:
-        player.play_wave(synthesizer.generate_constant_wave(freq, JSON_dict['audio_time']))
-        wave.append(synthesizer.generate_constant_wave(freq, JSON_dict['audio_time']))
+        player.play_wave(synthesizer.generate_constant_wave(freq, note_time * 2))
+        wave.append(synthesizer.generate_constant_wave(freq, note_time * 2))
 
 def file_check(exp_path): # check for existing file with the same name
     if os.path.exists(exp_path) == True: # if same named file exists, adds "- Copy" to the end
